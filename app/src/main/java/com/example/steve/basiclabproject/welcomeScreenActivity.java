18208d37b68c70c53768ac254e5790103a76afbc
teogodgift4 <teo.godgift4@gmail.com>
2017-10-15 22:09:04 +0300

    Welcome Screen final

--- app/src/main/java/com/example/steve/basiclabproject/welcomeScreenActivity.java
+++ app/src/main/java/com/example/steve/basiclabproject/welcomeScreenActivity.java
@@ -1,7 +1,9 @@
 package com.example.steve.basiclabproject;
 
+import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class welcomeScreenActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class welcomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcomescreenactivity);
+    }
+
+    public void exit(View view){
+       finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Process.killProcess(Process.myPid());
+        super.onDestroy();
     }
 }
