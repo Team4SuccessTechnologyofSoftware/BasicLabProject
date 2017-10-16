fcc2e969d0635c4e6c901adb3bb3d59fecbf896e
freaksoul <teo.godgift4@gmail.com>
2017-10-17 00:58:46 +0300

    Welcome Screen final -> passed JUnit tests

---
+++ app/src/test/java/com/example/steve/basiclabproject/welcomeScreenActivityTest.java
@@ -0,0 +1,38 @@
+package com.example.steve.basiclabproject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by freaksoul on 17/10/2017.
+ */
+public class welcomeScreenActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void exit() throws Exception {
+
+    }
+
+    @Test
+    public void onDestroy() throws Exception {
+
+    }
+
+}
