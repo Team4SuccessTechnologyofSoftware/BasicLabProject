97613e3f283b9f5c6046f86d22896ea464c909df
adis5330 <adis5330@hotmail.com>
2017-10-23 21:12:16 +0300

    database loginn and login screen

---
+++ app/src/main/java/com/example/steve/basiclabproject/SignIn.java
@@ -0,0 +1,87 @@
+package com.example.steve.basiclabproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignIn extends AppCompatActivity  implements View.OnClickListener {
+
+    private static final String REGISTER_URL = "https://team4success.000webhostapp.com/login_in.php";
+
+
+    public static final String KEY_USERNAME = "Username";
+    public static final String KEY_PASSWORD = "Password";
+
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_screen);
+        editTextUsername = (EditText) findViewById(R.id.TUserName);
+        editTextPassword = (EditText) findViewById(R.id.TPassword);
+        btn= (Button)findViewById(R.id.signinButton);
+        btn.setOnClickListener(this);
+    }
+
+    public void exit(View view){
+        finish();
+    }
+
+    private void signInUser(){
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(SignIn.this,response.toString(),Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(SignIn.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put(KEY_USERNAME, username);
+                params.put(KEY_PASSWORD,password);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btn){
+            signInUser();
+        }
+    }
+
+    }
+
--- app/src/main/java/com/example/steve/basiclabproject/welcomeScreenActivity.java
+++ app/src/main/java/com/example/steve/basiclabproject/welcomeScreenActivity.java
@@ -5,6 +5,8 @@ import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
 
 public class welcomeScreenActivity extends AppCompatActivity {
 
@@ -27,5 +29,12 @@ public class welcomeScreenActivity extends AppCompatActivity {
     protected void onDestroy() {
         Process.killProcess(Process.myPid());
         super.onDestroy();
+    }
+
+    //
+    public void Sign_In(View view) {
+        Intent intent= new Intent(welcomeScreenActivity.this,SignIn.class);
+       startActivity(intent);
+
     }
 }
