44fb930db8f010b7333fad44b27e0162c9b20f14
lefterislamp <firnen.1995@hotmail.com>
2017-10-29 20:31:23 +0200

    ArrayList για search categories + αρχικά layout

---
+++ app/src/main/java/com/example/steve/basiclabproject/Search.java
@@ -0,0 +1,14 @@
+package com.example.steve.basiclabproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchlayout);
+    }
+}
