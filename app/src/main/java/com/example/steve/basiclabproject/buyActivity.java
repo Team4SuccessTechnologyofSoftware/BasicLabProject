2de454d4d5285bfeb6050901cd09b2be69f6965f
teo <teo.godgift4@gmail.com>
2017-12-12 22:06:34 +0200

    bargain activity menu

--- app/src/main/java/com/example/steve/basiclabproject/buyActivity.java
+++ app/src/main/java/com/example/steve/basiclabproject/buyActivity.java
@@ -12,8 +12,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -38,6 +40,7 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -47,6 +50,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -55,6 +59,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static android.app.PendingIntent.getActivity;
 import static android.widget.Toast.LENGTH_LONG;
 import static java.lang.Double.valueOf;
 
@@ -75,20 +80,24 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
     List dlact = new ArrayList<Double>();
     List dlong = new ArrayList<Double>();
     List prodName;
-    List resp =new ArrayList();
-    List respIm= new ArrayList();
-    List markers =new ArrayList();
-    List priceList= new ArrayList();
-    String strPrDc="";
-    String helpEuros="";
+    List resp = new ArrayList();
+    List respIm = new ArrayList();
+    List markers = new ArrayList();
+    List priceList = new ArrayList();
+    String strPrDc = "";
+    String helpEuros = "";
     int[] id;
+    String subCat;
+    JSONArray objectArray;
+    int sumMarks;
+    GoogleMap stMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-        {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkLocationPermission();
         }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -98,8 +107,7 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
 
     }
 
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu, menu);
         return true;
@@ -110,6 +118,7 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
             case R.id.apartement:
                 if (item.isChecked()) item.setChecked(false);
                 else item.setChecked(true);
+                changeMapBySubCategory(item.getTitle().toString());
                 return true;
             case R.id.residence:
                 if (item.isChecked()) item.setChecked(false);
@@ -122,10 +131,12 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
             case R.id.cars:
                 if (item.isChecked()) item.setChecked(false);
                 else item.setChecked(true);
+                changeMapBySubCategory(item.getTitle().toString());
                 return true;
             case R.id.motorbikes:
                 if (item.isChecked()) item.setChecked(false);
                 else item.setChecked(true);
+                changeMapBySubCategory(item.getTitle().toString());
                 return true;
             case R.id.bicycles:
                 if (item.isChecked()) item.setChecked(false);
@@ -210,23 +221,18 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode)
-        {
+        switch (requestCode) {
             case REQUEST_LOCATION_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //permission is granted
-                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-                    {
-                        if (client == null)
-                        {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        if (client == null) {
                             buildGoogleApiClient();
                         }
                         mMap.setMyLocationEnabled(true);
-                    }
-                    else //permission denied
+                    } else //permission denied
                     {
-                        Toast.makeText(this, "Permission Denied!" , Toast.LENGTH_LONG).show();
+                        Toast.makeText(this, "Permission Denied!", Toast.LENGTH_LONG).show();
                     }
                     return;
                 }
@@ -247,69 +253,21 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
 
 
-
         mMap = googleMap;
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             buildGoogleApiClient();
             mMap.setMyLocationEnabled(true);
 
         }
         googleMap.setOnInfoWindowClickListener(this);
 
-        JsonArrayRequest request = new JsonArrayRequest("https://team4success.000webhostapp.com/getDetailsbargain.php",
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray jsonArray) {
-                        id= new int[jsonArray.length()];
-                        for(int i = 0; i < jsonArray.length(); i++) {
-                            try {
-                                id[i]= Integer.parseInt(jsonArray.getJSONObject(i).getString("productID"));
-                                Marker marker =googleMap.addMarker(new MarkerOptions().position((new LatLng(jsonArray.getJSONObject(i).getDouble("Lactitude"), jsonArray.getJSONObject(i).getDouble("Longtitude")))).title(jsonArray.getJSONObject(i).getString("ProductName")).snippet(jsonArray.getJSONObject(i).getString("price")+" Euros"));
-                                marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                                marker.setTag(jsonArray.getJSONObject(i));
-                            } catch (JSONException e) {
-                                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(buyActivity.this);
-                                dialogBuilder.setMessage("Error: " + e.getLocalizedMessage());
-                                dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        finish();
-                                    }
-                                });
-                                //Toast.makeText(buyActivity.this, "Error: " + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                                AlertDialog alertDialog= dialogBuilder.create();
-                                alertDialog.show();
-                            }
+        readyMapCustom();
 
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(buyActivity.this);
-                        dialogBuilder.setMessage("Unable to fetch data: " + volleyError.getMessage());
-                        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                finish();
-                            }
-                        });
-                        //Toast.makeText(buyActivity.this, "Unable to fetch data: " + volleyError.getMessage(), Toast.LENGTH_SHORT).show();
-                        AlertDialog alertDialog= dialogBuilder.create();
-                        alertDialog.show();
-                    }
-                });
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        requestQueue.add(request);
-
-        }
+    }
 
 
-    protected synchronized void buildGoogleApiClient()
-    {
+    protected synchronized void buildGoogleApiClient() {
         client = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -323,8 +281,7 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
     public void onLocationChanged(Location location) {
         lastLocation = location;
 
-        if (currentLocationMarker != null)
-        {
+        if (currentLocationMarker != null) {
             currentLocationMarker.remove();
         }
 
@@ -340,8 +297,7 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
 
-        if (client != null)
-        {
+        if (client != null) {
             LocationServices.FusedLocationApi.removeLocationUpdates(client, this);
         }
 
@@ -356,8 +312,7 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
         locationRequest.setFastestInterval(1000);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this);
 
         }
@@ -365,19 +320,15 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     public boolean checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
-            {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_CODE);
-            }
-            else {
+            } else {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_CODE);
 
             }
             return false;
-        }
-        else
+        } else
             return true;
     }
 
@@ -397,9 +348,9 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
     public void onInfoWindowClick(Marker marker) {
         Intent intent = new Intent(buyActivity.this, bargain.class);
         try {
-            intent.putExtra("Title",  marker.getTag().toString());
-        }catch (Exception e){
-            Toast.makeText(this,e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+            intent.putExtra("Title", marker.getTag().toString());
+        } catch (Exception e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
         }
         startActivity(intent);
     }
@@ -407,5 +358,79 @@ public class buyActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    public void changeMapBySubCategory(String subcategory) {
+        JSONObject obj;
+        Marker mar;
+        List marks = new ArrayList<Marker>();
+        for (int i = 0; i < markers.size(); i++) {
+            mar = (Marker) markers.get(i);
+            obj = (JSONObject) mar.getTag();
+            try {
+                if (!obj.getString("subcate").toString().equals(subcategory)) {
+                    marks.add(mar);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < marks.size(); i++) {
+           Marker marker = (Marker) marks.get(i);
+            marker.remove();
+        }
+        if(markers.size() == marks.size())
+            Toast.makeText(buyActivity.this,"Still no bargain for this kind of product...",Toast.LENGTH_LONG).show();
+    }
+
+
+    public void readyMapCustom(){
+        JsonArrayRequest request = new JsonArrayRequest("https://team4success.000webhostapp.com/getDetailsbargain.php",
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
+                        id = new int[jsonArray.length()];
+                        objectArray = jsonArray;
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            try {
+                                id[i] = Integer.parseInt(jsonArray.getJSONObject(i).getString("productID"));
+                                Marker marker = mMap.addMarker(new MarkerOptions().position((new LatLng(jsonArray.getJSONObject(i).getDouble("Lactitude"), jsonArray.getJSONObject(i).getDouble("Longtitude")))).title(jsonArray.getJSONObject(i).getString("ProductName")).snippet(jsonArray.getJSONObject(i).getString("price") + " Euros"));
+                                marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                                marker.setTag(jsonArray.getJSONObject(i));
+                                markers.add(marker);
+                            } catch (JSONException e) {
+                                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(buyActivity.this);
+                                dialogBuilder.setMessage("Error: " + e.getLocalizedMessage());
+                                dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                });
+                                //Toast.makeText(buyActivity.this, "Error: " + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                                AlertDialog alertDialog = dialogBuilder.create();
+                                alertDialog.show();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(buyActivity.this);
+                        dialogBuilder.setMessage("Unable to fetch data: " + volleyError.getMessage());
+                        dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                        //Toast.makeText(buyActivity.this, "Unable to fetch data: " + volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                        AlertDialog alertDialog = dialogBuilder.create();
+                        alertDialog.show();
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
